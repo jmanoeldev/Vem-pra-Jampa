bc2a1094a2a8b4e7425195f58cc6d9c049fcf37f
José Manoel <jose1manoel2silva@gmail.com>
2026-06-08 11:03:06 -0300

    fix/Corrigi chamadas no formulario para se conectar corretamente com as funções de cadastro e login

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SECRET_KEY']='petronio-fofinho'
 
+
 from app import routes
 from app.autenticacao import auth
 app.register_blueprint(auth)
