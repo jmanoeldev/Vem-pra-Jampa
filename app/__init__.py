aefeacd808d601fcedb9bb2b241805b1c25c45a1
Mateus Menezes <m.souza0913@gmail.com>
2026-06-10 20:39:17 -0300

    fix: Remove detalhes de produção.
    -Exclui atribuições indesejadas.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
-app.config['SECRET_KEY']='petronio-fofinho'
+app.config['SECRET_KEY']='vem-pra-jampa-2026'
 
 
 from app import routes
